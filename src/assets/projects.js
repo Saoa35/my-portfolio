e6d8f6bfd6c31096d3c4d7ac803f0395296eb6a7
Saoa35 <saoamah@gmail.com>
2023-05-18 19:21:22 +0400

    created interface Project

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -1,20 +1,20 @@
-import project01 from "./../img/projects/01.jpg";
-import project01Big from "./../img/projects/01-big.jpg";
+import project01 from "./../img/projects/01.jpeg";
+import project01Big from "./../img/projects/01-big.jpeg";
 
-import project02 from "./../img/projects/02.jpg";
-import project02Big from "./../img/projects/02-big.jpg";
+import project02 from "./../img/projects/02.jpeg";
+import project02Big from "./../img/projects/02-big.jpeg";
 
-import project03 from "./../img/projects/03.jpg";
-import project03Big from "./../img/projects/03-big.jpg";
+import project03 from "./../img/projects/03.jpeg";
+import project03Big from "./../img/projects/03-big.jpeg";
 
-import project04 from "./../img/projects/04.jpg";
-import project04Big from "./../img/projects/04-big.jpg";
+import project04 from "./../img/projects/04.jpeg";
+import project04Big from "./../img/projects/04-big.jpeg";
 
-import project05 from "./../img/projects/05.jpg";
-import project05Big from "./../img/projects/05-big.jpg";
+import project05 from "./../img/projects/05.jpeg";
+import project05Big from "./../img/projects/05-big.jpeg";
 
-import project06 from "./../img/projects/06.jpg";
-import project06Big from "./../img/projects/06-big.jpg";
+import project06 from "./../img/projects/06.jpeg";
+import project06Big from "./../img/projects/06-big.jpeg";
 
 const projects = [
   {
@@ -29,7 +29,7 @@ const projects = [
   {
     id: 2,
     title: "Online shop website",
-    skills:
+    technologies:
       "ReactJS + hooks, React Router, Axios, SCSS, React Context, React Skeleton",
     img: project02,
     gitHubLink: "https://github.com/Saoa35/model_shop",
@@ -37,28 +37,28 @@ const projects = [
   {
     id: 3,
     title: "Photo gallery website",
-    skills: "HTML, SCSS, ReactJS, React Hooks, Pagination",
+    technologies: "HTML, SCSS, ReactJS, React Hooks, Pagination",
     img: project03,
     gitHubLink: "https://github.com/Saoa35/photo_gallery",
   },
   {
     id: 4,
     title: "Food ordering and delivery website",
-    skills: "HTML5, CSS3, SCSS, JavaScript, Gulp, Firebase",
+    technologies: "HTML5, CSS3, SCSS, JavaScript, Gulp, Firebase",
     img: project04,
     gitHubLink: "https://github.com/Saoa35/Food-Restaurants",
   },
   {
     id: 5,
     title: "Simple weather forecast",
-    skills: "HTML5, CSS3, JavaScript",
+    technologies: "HTML5, CSS3, JavaScript",
     img: project05,
     gitHubLink: "https://github.com/Saoa35/Weather-Forecast",
   },
   {
     id: 6,
     title: "Simple Notes app",
-    skills: "HTML5, CSS3, SCSS, ReactJS, React Hooks",
+    technologies: "HTML5, CSS3, SCSS, ReactJS, React Hooks",
     img: project06,
     gitHubLink: "https://github.com/Saoa35/oblicksoft",
   },
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,52 +1,24 @@
 import React from "react";
-
 import { ProjectCard } from "../components/ProjectCard/ProjectCard.tsx";
-
-// import project01 from "./../img/projects/01.jpg";
-// import project02 from "./../img/projects/02.jpg";
-// import project03 from "./../img/projects/03.jpg";
-// import project04 from "./../img/projects/04.jpg";
-// import project05 from "./../img/projects/05.jpg";
-// import project06 from "./../img/projects/06.jpg";
+import { projects } from "./../assets/projects.js";
 
 export const Projects = (): React.JSX.Element => {
+  interface Project {
+    id: number;
+    title: string;
+    technologies: string;
+    img: any;
+    gitHubLink: string;
+  }
+
   return (
     <main className="section">
       <div className="container">
         <h2 className="title-1">Projects</h2>
         <ul className="projects">
-          <ProjectCard />
-          {/* <li className="project">
-            <a href="./project-page.html">
-              <img src={project01} alt="Project img" className="project__img" />
-              <h3 className="project__title">Gaming streaming portal</h3>
-            </a>
-          </li> */}
-          {/* <li className="project">
-            <a href="./project-page.html">
-              <img src={project02} alt="Project img" className="project__img" />
-              <h3 className="project__title">Video service</h3>
-            </a>
-          </li>
-          <li className="project">
-            <a href="./project-page.html">
-              <img src={project03} alt="Project img" className="project__img" />
-              <h3 className="project__title">Video portal</h3>
-            </a>
-          </li>
-
-          <li className="project">
-            <img src={project04} alt="Project img" className="project__img" />
-            <h3 className="project__title">Dating app</h3>
-          </li>
-          <li className="project">
-            <img src={project05} alt="Project img" className="project__img" />
-            <h3 className="project__title">Landing</h3>
-          </li>
-          <li className="project">
-            <img src={project06} alt="Project img" className="project__img" />
-            <h3 className="project__title">Gaming community</h3>
-          </li> */}
+          {projects.map((el: Project) => {
+            return <ProjectCard {...el} />;
+          })}
         </ul>
       </div>
     </main>
