725b038639be63901259b0f79eb85f89175bea5a
Saoa35 <saoamah@gmail.com>
2023-05-20 15:57:53 +0400

    changed logic for rendering social links

--- src/components/DarkButton/DarkButton.tsx
+++ src/components/DarkButton/DarkButton.tsx
@@ -1,17 +1,22 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./style.scss";
 
 import sun from "./../../img/icons/sun.svg";
 import moon from "./../../img/icons/moon.svg";
 
 export const DarkButton = (): React.JSX.Element => {
-  const [darkMode, setDarkMode] = useState("light");
+  const [darkMode, setDarkMode] = useState<DarkMode>("light");
+  const btnRef = useRef<HTMLButtonElement>(null);
+
+  type DarkMode = "light" | "dark";
 
   useEffect(() => {
     if (darkMode === "dark") {
       document.body.classList.add("dark");
+      btnRef.current?.classList.add("dark-mode-btn--active");
     } else {
       document.body.classList.remove("dark");
+      btnRef.current?.classList.remove("dark-mode-btn--active");
     }
   }, [darkMode]);
 
@@ -22,7 +27,7 @@ export const DarkButton = (): React.JSX.Element => {
   };
 
   return (
-    <button className="dark-mode-btn" onClick={toggleMode}>
+    <button ref={btnRef} className="dark-mode-btn" onClick={toggleMode}>
       <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
       <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
     </button>
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,46 +1,44 @@
 import React from "react";
 import "./style.css";
 
-import vk from "./../../img/icons/vk.svg";
-import twitter from "./../../img/icons/twitter.svg";
+import telegram from "./../../img/icons/telegram.svg";
+import skype from "./../../img/icons/skype.svg";
 import linkedIn from "./../../img/icons/linkedIn.svg";
-import instagram from "./../../img/icons/instagram.svg";
+import whatsapp from "./../../img/icons/whatsapp.svg";
 import gitHub from "./../../img/icons/gitHub.svg";
 
 export const Footer = (): React.JSX.Element => {
+  const social = [
+    { link: "https://join.skype.com/invite/Akb6lZbgAL2D", image: skype },
+    {
+      link: "https://www.linkedin.com/in/oleksandr-makharadze/",
+      image: linkedIn,
+    },
+    {
+      link: "https://web.whatsapp.com/send/?phone=380666784893",
+      image: whatsapp,
+    },
+    { link: "https://github.com/Saoa35", image: gitHub },
+    { link: "https://t.me/Alexandr_Makharadze", image: telegram },
+  ];
+
   return (
     <footer className="footer">
       <div className="container">
         <div className="footer__wrapper">
           <ul className="social">
-            <li className="social__item">
-              <a href="#!">
-                <img src={vk} alt="Link" />
-              </a>
-            </li>
-            <li className="social__item">
-              <a href="#!">
-                <img src={twitter} alt="Link" />
-              </a>
-            </li>
-            <li className="social__item">
-              <a href="#!">
-                <img src={linkedIn} alt="Link" />
-              </a>
-            </li>
-            <li className="social__item">
-              <a href="#!">
-                <img src={instagram} alt="Link" />
-              </a>
-            </li>
-            <li className="social__item">
-              <a href="#!">
-                <img src={gitHub} alt="Link" />
-              </a>
-            </li>
+            {social.map((obj, i) => {
+              return (
+                <li className="social__item" key={i}>
+                  <a href={obj.link} target="_blank" rel="noreferrer">
+                    <img src={obj.image} alt="Link" />
+                  </a>
+                </li>
+              );
+            })}
           </ul>
           <div className="copyright">
-            <p>© 2023 frontend-dev.com</p>
+            <p>© 2023 web-development</p>
           </div>
         </div>
       </div>
