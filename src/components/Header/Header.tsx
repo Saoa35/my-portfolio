f582415a80aaea896de95b06c2c790fca793602a
Saoa35 <saoamah@gmail.com>
2023-05-20 16:54:30 +0400

    added current linck to CV button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -14,7 +14,10 @@ export const Header = (): React.JSX.Element => {
         <div className="header__text">
           <p>with passion for learning and creating.</p>
         </div>
-        <a href="#!" className="btn">
+        <a
+          href="https://drive.google.com/file/d/1H-7zheSrfNfUyhCcPv4_gOcuz-J-eFOr/view?usp=sharing"
+          className="btn"
+        >
           Download CV
         </a>
       </div>
