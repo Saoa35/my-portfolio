bcb3eebc3144602f4e67891d9b4e8440ddb8cd3c
Saoa35 <saoamah@gmail.com>
2023-07-05 15:40:14 +0400

    + Simple Chat project

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -2,13 +2,20 @@ import burger from "./../img/projects/burger-lord.png";
 import models from "./../img/projects/model-shop.png";
 import gallery from "./../img/projects/foto-gallery.png";
 import food from "./../img/projects/food-restaurant.png";
-import forecast from "./../img/projects/forecast.png";
 import notes from "./../img/projects/notes.png";
 import travel from "./../img/projects/travel-blog.png";
+import chat from "./../img/projects/chat1.png";
 
 const projects = [
   {
     id: 0,
+    title: "Simple Chat website",
+    technologies: "HTML, CSS, JS, React, NodeJS, Express, Socket IO",
+    img: chat,
+    gitHubLink: "https://github.com/Saoa35/Simple_Chat",
+  },
+  {
+    id: 1,
     title: "Travel website",
     technologies:
       "HTML, CSS, Tailwind, JS, React, ReactHooks, ReactRouterDom, ReduxToolkit, NodeJS, MongoDB.",
@@ -16,7 +23,7 @@ const projects = [
     gitHubLink: "https://github.com/Saoa35/Travel-Blog",
   },
   {
-    id: 1,
+    id: 2,
     title: "Burger restaurant website",
     technologies:
       "ReactJS, TypeScript, Redux Toolkit, React Router, Axios + Fetch, React Hooks, CSS-Modules / SCSS, React Content Loader, React Pagination, Lodash.Debounce, Code Splitting, React Loadable, useWhyDidYouUpdate",
@@ -24,7 +31,7 @@ const projects = [
     gitHubLink: "https://github.com/Saoa35/burger_lord",
   },
   {
-    id: 2,
+    id: 3,
     title: "Online shop website",
     technologies:
       "ReactJS + hooks, React Router, Axios, SCSS, React Context, React Skeleton",
@@ -32,25 +39,18 @@ const projects = [
     gitHubLink: "https://github.com/Saoa35/model_shop",
   },
   {
-    id: 3,
+    id: 4,
     title: "Photo gallery website",
     technologies: "HTML, SCSS, ReactJS, React Hooks, Pagination",
     img: gallery,
     gitHubLink: "https://github.com/Saoa35/photo_gallery",
   },
   {
-    id: 4,
+    id: 5,
     title: "Food ordering and delivery website",
     technologies: "HTML5, CSS3, SCSS, JavaScript, Gulp, Firebase",
     img: food,
     gitHubLink: "https://github.com/Saoa35/Food-Restaurants",
-  },
-  {
-    id: 5,
-    title: "Simple weather forecast",
-    technologies: "HTML5, CSS3, JavaScript",
-    img: forecast,
-    gitHubLink: "https://github.com/Saoa35/Weather-Forecast",
   },
   {
     id: 6,
